519388dc3c34aa7e390da96456769e644b47bfd9
suryansh rathore <suryansh@metadrob.com>
2024-07-24 17:47:41 +0530

    databases connections

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// require("dotenv").config({path: "./env"});
+// privious version
+
+//new version
+import dotenv from "dotenv"
+
+
+import mongoose from "mongoose";
+// import {DB_NAME} from "./constants";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env"
+});
+
+connectDB();
+
+//1st approach
+
+// (async ()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     }
+//     catch (error){
+//         console. log("ERROR", error);
+//         throw error;
+//     }
+// })()
